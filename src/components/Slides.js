f24c4d3ef0ddbf9d0b470cb5ad98019d2a9f971a
Ongun <info@ongunakay.com>
2025-06-20 20:37:36 +0300

    //

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Slides({ slides }) {
+  const [index, setIndex] = useState(0);
+
+  const restart = () => setIndex(0);
+  const next = () => setIndex(index + 1);
+  const prev = () => setIndex(index - 1);
+
   return (
     <div>
       <div id="navigation" className="text-center">
-        <button data-testid="button-restart" className="small outlined">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          onClick={restart}
+          disabled={index === 0}
+        >
           Restart
         </button>
-        <button data-testid="button-prev" className="small">
+        <button
+          data-testid="button-prev"
+          className="small"
+          onClick={prev}
+          disabled={index === 0}
+        >
           Prev
         </button>
-        <button data-testid="button-next" className="small">
+        <button
+          data-testid="button-next"
+          className="small"
+          onClick={next}
+          disabled={index === slides.length - 1}
+        >
           Next
         </button>
       </div>
       <div id="slide" className="card text-center">
-        <h1 data-testid="title">Slide Title Here</h1>
-        <p data-testid="text">Slide Text Here</p>
+        <h1 data-testid="title">{slides[index].title}</h1>
+        <p data-testid="text">{slides[index].text}</p>
       </div>
     </div>
   );
